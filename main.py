b2f6a6ee209a7880304d5f5a35230e7f28ccbbe5
MGonza20 <sara.paguaga@gmail.com>
2023-03-13 10:30:13 -0600

    Refactoring

--- main.py
+++ main.py
@@ -9,12 +9,18 @@ DATASET_SPARSE_RATIO = 20
 DATASET_X_LIM = 10
 
 # Imaginemos que X es dado
-X = np.linspace(0, DATASET_X_LIM, DATASET_SIZE).reshape((DATASET_SIZE, 1))
-Xr = np.hstack((
-    np.ones((DATASET_SIZE, 1)),
-    X
-))
-y = 3 + 2 * X + np.random.rand(DATASET_SIZE, 1) * DATASET_SPARSE_RATIO
+
+def get_random_dataset():
+    X = np.linspace(0, DATASET_X_LIM, DATASET_SIZE).reshape((DATASET_SIZE, 1))
+    Xr = np.hstack((
+        np.ones((DATASET_SIZE, 1)),
+        X
+    ))
+    y = 3 + 2 * X + np.random.rand(DATASET_SIZE, 1) * DATASET_SPARSE_RATIO
+    return Xr, y
+
+
+Xr, y = get_random_dataset()
 to = np.random.rand(Xr.shape[1], 1)
 
 tf, costs = linreg(
@@ -36,7 +42,7 @@ xmr = np.hstack((
 ))
 ym = xmr @ tf
 
-plt.plot(X, y, 'ro')
+plt.plot(Xr[:, 1], y, 'ro')
 plt.plot(xm, ym)
 plt.show()
 
