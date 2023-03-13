97b350e078b49ecaa22e0f1cccf593c0ef4fc52e
MGonza20 <sara.paguaga@gmail.com>
2023-03-13 10:03:26 -0600

    dibujando cambios

--- main.py
+++ main.py
@@ -16,16 +16,32 @@ Xr = np.hstack((
 ))
 y = 3 + 2 * X + np.random.rand(DATASET_SIZE, 1) * DATASET_SPARSE_RATIO
 to = np.random.rand(Xr.shape[1], 1)
-tf = linreg(Xr, y, to, quad.cost, quad.grad, a=0.025, n=500)
+
+def draw_each_step(t):     
+    xm = np.array([[0], [DATASET_X_LIM]])
+    xmr = np.hstack((
+        np.ones((2, 1)),
+        xm
+    ))
+    ym = xmr @ t
+    plt.plot(X, y, 'ro')
+    plt.plot(xm, ym)
+    plt.show()
+
+tf = linreg(
+    Xr, 
+    y, 
+    to, 
+    quad.cost, 
+    quad.grad, 
+    a=0.025, 
+    n=20,
+    on_step=draw_each_step
+)
+
 print("Tf: ", tf)
 
-xm = np.array([[1], [DATASET_X_LIM]])
-xmr = np.hstack((
-    np.ones((2, 1)),
-    xm
-))
-ym = xmr @ tf
 
-plt.plot(X, y, 'ro')
-plt.plot(xm, ym)
-plt.show()
+# plt.plot(X, y, 'ro')
+# plt.plot(xm, ym)
+# plt.show()
